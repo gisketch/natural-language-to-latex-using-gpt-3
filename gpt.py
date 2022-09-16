a8540bb29586806e8a38ced91e5c32b8156b6860
gisketch <agjmnz@gmail.com>
2022-09-16 15:52:45 +0800

    added gpt latex class and a new prompt

--- gpt.py
+++ gpt.py
@@ -1,38 +1,71 @@
 import openai
 import os
+import json
 from dotenv import load_dotenv
 
 class GPT:
     def __init__(self):
         load_dotenv()
         openai.api_key = os.getenv("GPT_KEY")
+        
+    class Latex_GPT:
+        def __init__ (self, input):
+            load_dotenv()
+            openai.api_key = os.getenv("GPT_KEY")
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=r"""The following is a conversation with a personal AI assistant that parses mathematical expressions from natural language in LatEx.
 
-    def latex(self, input):
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=r"""You are an AI that converts natural language into mathematical expressions using LatEx.
+Some information about the AI:
 
+- Created on August 31, 2022 by "Ghegi Jimenez".
+- A product of Ghegi's thesis project.
+- Programmed in Python.
+- Powered by OpenAI's GPT-3 models.
+- AI is nicknamed "Zelda" inspired by the game "The Legend of Zelda"
 
-User: What is the integral of 2 x plus 3 from 0 to 9?
-LatEx: \int_0^9 2x + 3 \, dx
+Rules Zelda follows:
 
-User: What is the area under the curve of y=2x+3 from x=0 to x=9?
-LatEx: \int_0^9 2x + 3 \, dx
+- Responds with a JSON object containing the converted LatEx expression (IF AVAILABLE), and Zelda's response in text.
 
-User: Can you write the equation of a line in slope-intercept form that passes through the points (1,2) and (3,5)?
-LatEx: y = \frac{1}{2}x + \frac{3}{2}
+EXAMPLE:
+{"response": <<zelda's response>>, "latex": <<latex>> *do not solve, always write what is wanted from the user }
 
-User: What would be the equation for a line that is parallel to y=2x+3 and passes through the point (1,5)?
-LatEx: y = 2x + 5
+- Does not try to solve the equations, only converts them to LatEx.
+- Do not make up equations. Always ask for the specific equations. Only exception is if asked for a random equation.
 
-User: The limit of x squared plus 3x plus 4 divided by 9x as x approaches 3
-LatEx: \lim_{x \to 3} \frac{x^2 + 3x + 4}{9x}
-"""
-+ f"User: {input}\nLatEx: ",
-            temperature=0,
-            max_tokens=512,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-        return response.choices[0].text
+<--The following is a continuation of their previous conversation-->
+Human: Hello Zelda!
+Zelda: {"response":"Hello, what can I do  for you?"}
+Human: Can you help me  with my math homework?
+Zelda: {"response":"I can try! What do you need help with?"}
+Human: Write this for me, x squared plus 3 x divided by three
+Zelda: {"response":"Okay, this is what I came up with.", "latex": "x^2+3x/3"}
+Human: Thank you!
+Zelda: {"response":"You're welcome!"}
+Human: How about two plus two?
+Zelda: {"response":"Here's what I got", "latex": "2+2"}
+Human: Can you write the quadratic formula for me?
+Zelda: {"response":"Here you go!", "latex": "x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}"}
+Human: Write the integral of x squared from a to b
+Zelda: {"response":"Here's the integral", "latex": "\\int_{a}^{b}x^2dx"}
+Human: Write the limit of pi x plus pi y divided by sine pi as x approaches to 2 pi
+Zelda: {"response":"Here's the limit", "latex": "\\lim_{x\\to2\\pi}\frac{\\pi x+\\pi y}{\\sin \\pi}"}
+Human: What is the derivative of x cubed plus 3 x squared?
+Zelda: {"response":"I can't solve for the derivatives but I can parse that equation.", "latex": "x^3+3x^2"}
+Human: Oh okay, thank you!
+Zelda: {"response":"You're welcome!"}
+Human: """ + input + "\nZelda:",
+                temperature=0,
+                max_tokens=1500,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+            )
+            output_file = json.loads(response.choices[0].text)
+            print(output_file)
+            self.response = output_file["response"]
+            if "latex" in output_file:
+                self.latex = output_file["latex"]
+            else:
+                self.latex = "none"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import gpt
 import requests
-import os
+import io
+import matplotlib.pyplot as plt
+from PIL import Image, ImageChops
 
 class Zelda:
     def __init__(self):
@@ -10,14 +12,23 @@ class Zelda:
         with open(path, 'w') as file:
             file.write(data)
 
-    def tex_to_png(self, str): #function that saves txt to tex to png
-        url = "https://math.vercel.app/?from=" + str.replace(" ", "%20").replace("+", "%2B")
+    def save_svg(self, str): #function that saves txt to tex to png
+        url = "https://math.vercel.app/?from=" + str
         svg = requests.get(url).text
         self.write_text(svg, "./temp/latex.svg")
+    
+    def latex_to_img(self, tex):
+        plt.rc('text', usetex=False)
+        plt.axis('off')
+        plt.text(0.05, 0.5, f'${tex}$', size=60)
+        # plt.savefig("img.png", format='png')
+        plt.show()
+        plt.close()
 
     def latex(self, input):
-        tex = self.gpt.latex(input)
-        self.tex_to_png(tex)
+        self.latex_to_img(input)
 
 zelda = Zelda()
-zelda.latex(input("Command: "))
+latexgpt = zelda.gpt.Latex_GPT(input("Command: "))
+print(latexgpt.response + " " + latexgpt.latex)
+zelda.latex_to_img(latexgpt.latex)
